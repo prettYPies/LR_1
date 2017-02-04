4081acc4d50bd3c13e228874a4525d4b596aa7d7
prettYPies <lilya@malyuta.pro>
2017-02-04 22:03:56 +0300

    full ex4

--- 4.py
+++ 4.py
@@ -4,5 +4,15 @@ Created on Sat Feb  4 18:23:16 2017
 
 @author: joe27
 """
-
+i = 0; 
 inText = input("Введите текст: ")
+splitText = inText.split()
+print(splitText)
+
+def sortByLength(splitText):
+        return len(splitText) # Ключом является длина каждой строки, сортируем по длине  
+splitText.sort(key=sortByLength, reverse=True)
+
+while i < len(splitText):
+    print(splitText[i])
+    i = i + 1
